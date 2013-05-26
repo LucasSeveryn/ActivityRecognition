3f72a47f31381abe221c60adb4bfccf791897c3a
LucasSeveryn <moa.1991@gmail.com>
2013-05-26 05:59:55 +0100

    Classification probabilities are now displayed in graph form.

--- src/com/severyn/actrecognition/ActRecognitionFragment.java
+++ src/com/severyn/actrecognition/ActRecognitionFragment.java
@@ -1,10 +1,24 @@
 package com.severyn.actrecognition;
 
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+import com.androidplot.ui.AnchorPosition;
+import com.androidplot.xy.BarFormatter;
+import com.androidplot.xy.BarRenderer;
 import com.androidplot.xy.BoundaryMode;
+import com.androidplot.xy.LineAndPointFormatter;
+import com.androidplot.xy.SimpleXYSeries;
+import com.androidplot.xy.XLayoutStyle;
 import com.androidplot.xy.XYPlot;
 import com.androidplot.xy.XYStepMode;
+import com.androidplot.xy.YLayoutStyle;
 import com.example.actrecognition.R;
 
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -13,7 +27,8 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class ActRecognitionFragment extends Fragment {
-	XYPlot xyzPlot;
+	XYPlot pPlot = null;
+	private SimpleXYSeries pSeries = null;
 
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
@@ -22,22 +37,70 @@ public class ActRecognitionFragment extends Fragment {
 
 		((MainActivity) getActivity()).setTabFragment(3, getTag());
 
-		xyzPlot = (XYPlot) rootView.findViewById(R.id.xyzPlot);
-		xyzPlot.setRangeBoundaries(-15, 15, BoundaryMode.FIXED);
-		xyzPlot.setRangeStep(XYStepMode.INCREMENT_BY_VAL, 2);
-		xyzPlot.setDomainBoundaries(0, 512, BoundaryMode.FIXED);
-		xyzPlot.setTicksPerRangeLabel(1);
-		xyzPlot.setDomainLabel("Time");
-		xyzPlot.getDomainLabelWidget().pack();
-		xyzPlot.setRangeLabel("Acceleration");
-		xyzPlot.getRangeLabelWidget().pack();
-		xyzPlot.setTitle("Recording");
+		pPlot = (XYPlot) rootView.findViewById(R.id.pPlot);
+		
+		
+		pSeries = new SimpleXYSeries("Probability");
+		pSeries.useImplicitXVals();
+		pPlot.addSeries(pSeries, new BarFormatter(Color.argb(100, 51, 132, 187), Color.rgb(11, 108, 174)));
+		
+//		pPlot.setRangeBoundaries(, 15, BoundaryMode.FIXED);
+//		xyzPlot.setRangeStep(XYStepMode.INCREMENT_BY_VAL, 2);
+		pPlot.setDomainBoundaries(-0.5, 8.5, BoundaryMode.FIXED);
+		pPlot.setDomainStep(XYStepMode.INCREMENT_BY_VAL, 1);
+//		xyzPlot.setTicksPerRangeLabel(1);
+		pPlot.setDomainLabel("Type");
+		pPlot.getDomainLabelWidget().pack();
+		pPlot.setRangeLabel("Probability");
+		pPlot.getRangeLabelWidget().pack();
+		pPlot.setTitle("Classification Results");
+		pPlot.getLayoutManager().remove(pPlot.getLegendWidget());
+		pPlot.setRangeValueFormat(new DecimalFormat("#"));
 
-		xyzPlot.redraw();
+	     BarRenderer barRenderer = (BarRenderer) pPlot.getRenderer(BarRenderer.class);
+	       if(barRenderer != null) {
+	           // make our bars a little thicker than the default so they can be seen better:
+	           barRenderer.setBarWidth(60);
+	       }
 
+		pPlot.redraw();
+		
 		return rootView;
 	}
 
+	public void drawData(ArrayList<Double> results) {
+		ArrayList<Double> probabilityData = new ArrayList<Double>();
+		
+		for(int i=0;i<results.size();i++){
+			if (!Double.isInfinite(results.get(i)) && i != 1 && i != 4 && i != 5 && i != 6) {
+				probabilityData.add(results.get(i));			
+				//probabilityData.add(Math.exp(results.get(i)));
+			}
+			else probabilityData.add(0.0);
+		}
+		
+		ArrayList<Double> probabilityData2 = new ArrayList<Double>();
+
+		for(int i=0;i<probabilityData.size();i++){
+			if (probabilityData.get(i)==0.0){
+				probabilityData2.add(Collections.min(probabilityData)*1.5);
+			}else{
+				probabilityData2.add(probabilityData.get(i));
+			}
+
+		}
+		
+		pPlot.setRangeBoundaries(Collections.min(probabilityData)*1.5,0, BoundaryMode.FIXED);
+//		pPlot.setRangeValueFormat(new DecimalFormat("0000E0"));
+		pSeries.setModel(probabilityData2,
+				SimpleXYSeries.ArrayFormat.Y_VALS_ONLY);
+
+
+		pPlot.redraw();
+
+	}
+	
+	
 	public void updateStatusText(String text, boolean append) {
 		TextView statusText = (TextView) this.getView().findViewById(
 				R.id.statusText);

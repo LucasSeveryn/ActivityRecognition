bcc948d48784a3c272c9de3ff598dd6a87f911e8
LucasSeveryn <moa.1991@gmail.com>
2013-05-26 05:53:17 +0100

    Classifier now displays both decimal and log probability

--- src/com/severyn/actrecognition/NaiveGaussianClassifier.java
+++ src/com/severyn/actrecognition/NaiveGaussianClassifier.java
@@ -1,24 +1,23 @@
 package com.severyn.actrecognition;
+
 import java.util.ArrayList;
 
 import android.util.Pair;
-
 
 public class NaiveGaussianClassifier {
 	ArrayList<ArrayList<Double>> entropyMean;
 	ArrayList<ArrayList<Double>> entropyVar;
 
-	
-
-	public NaiveGaussianClassifier(ArrayList<ArrayList<Double>> entropyMean, ArrayList<ArrayList<Double>> entropyVar) {
+	public NaiveGaussianClassifier(ArrayList<ArrayList<Double>> entropyMean,
+			ArrayList<ArrayList<Double>> entropyVar) {
 		this.entropyMean = entropyMean;
 		this.entropyVar = entropyVar;
-        
+
 	}
 
-	public Pair<Integer,String> classify(AccFeat q) {
-		String txt="";
-		txt+=("\n- Starting NBC Classification - may25");
+	public Pair<ArrayList<Double>, String> classify(AccFeat q) {
+		String txt = "";
+		txt += ("\n- Starting NBC Classification - may25");
 		double[] results = new double[9];
 		double result;
 		ArrayList<Double> qf = new ArrayList<Double>();
@@ -29,13 +28,16 @@ public class NaiveGaussianClassifier {
 
 		for (int i = 0; i < 9; i++) {
 			if (i != 1 && i != 4 && i != 5 && i != 6) { // debug
-//				result = 1;
+			// result = 1;
 				result = 0;
 				for (int j = 0; j < entropyMean.get(i).size(); j++) {
-//					result = result * p(qf.get(j), entropyMean.get(i).get(j),entropyVar.get(i).get(j));
-					//result = result - Math.log(p(qf.get(j), entropyMean.get(i).get(j),entropyVar.get(i).get(j)));
-					result +=Math.log(p(qf.get(j), entropyMean.get(i).get(j),entropyVar.get(i).get(j)));
-					
+					// result = result * p(qf.get(j),
+					// entropyMean.get(i).get(j),entropyVar.get(i).get(j));
+					// result = result - Math.log(p(qf.get(j),
+					// entropyMean.get(i).get(j),entropyVar.get(i).get(j)));
+					result += Math.log(p(qf.get(j), entropyMean.get(i).get(j),
+							entropyVar.get(i).get(j)));
+
 				}
 				results[i] = result;
 			}
@@ -44,52 +46,51 @@ public class NaiveGaussianClassifier {
 		int maxindex = 0;
 		double maxvalue = results[0];
 
-		for(int i=0;i<9;i++){
-			if(i!= 1 && i != 4 && i != 5 && i != 6&&!Double.isNaN(results[i])){
-				maxvalue=results[i];
-				maxindex=i;
+		for (int i = 0; i < 9; i++) {
+			if (i != 1 && i != 4 && i != 5 && i != 6
+					&& !Double.isNaN(results[i])) {
+				maxvalue = results[i];
+				maxindex = i;
 				break;
 			}
 		}
-		
+
 		for (int i = 0; i < 9; i++) {
-			
-			
-			
-			if (!Double.isNaN(results[i])){
-				
-				
-				if(results[i]!=0.0){
-					txt+=("\n["+i+"] "+Math.exp(results[i]));
+			if (!Double.isNaN(results[i])) {
+
+				if (results[i] != 0.0) {
+					txt += ("\n[" + i + "] " + String.format("%.5f", Math.exp(results[i]))+ " log:" + String.format("%.5f", results[i]));
 				}
-				if(results[i] > results[maxindex] && i != 1 && i != 4 && i != 5 && i != 6) {
+				if (results[i] > results[maxindex] && i != 1 && i != 4
+						&& i != 5 && i != 6) {
 					maxvalue = results[i];
 					maxindex = i;
 				}
 			}
-				
-			}
+
+		}
 
 		for (int i = 0; i < results.length; i++) {
 			if (i != maxindex && i != 1 && i != 4 && i != 5 && i != 6) {
-				txt+=("\n   Type #"
-						+ i
-						+ " : "   
-						+ String.format("%.2f", 
-								results[i] / results[maxindex]  )
-						+ " times less likely.");
+				txt += ("\n   Type #" + i + " : "
+						+ String.format("%.2f", results[i] / results[maxindex]) + " times less likely.");
 			}
 		}
-		txt+=("\n");
+		txt += ("\n");
 
-		txt+=("\n- This is an activity of type #" + maxindex);
+		txt += ("\n- This is an activity of type #" + maxindex);
 
-	
-		Pair<Integer, String> pair = new Pair<Integer, String>(maxindex, txt);
+		ArrayList<Double> resultsArrayList = new ArrayList<Double>();
+		for (Double d : results) {
+			resultsArrayList.add(d);
+		}
+
+		Pair<ArrayList<Double>, String> pair = new Pair<ArrayList<Double>, String>(
+				resultsArrayList, txt);
 		return pair;
 
 	}
-	
+
 	private double p(double v, double m, double var) {
 		if (var == 0)
 			return 1;
@@ -99,13 +100,12 @@ public class NaiveGaussianClassifier {
 
 		return p;
 	}
-	
 
-	public ArrayList<ArrayList<Double>> getEntropyMean(){
+	public ArrayList<ArrayList<Double>> getEntropyMean() {
 		return entropyMean;
 	}
-	
-	public ArrayList<ArrayList<Double>> getEntropyVar(){
+
+	public ArrayList<ArrayList<Double>> getEntropyVar() {
 		return entropyVar;
 	}
 }
